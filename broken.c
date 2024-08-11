2dfea406f3dd7458e63feb80ba01875815953686
SK <transiencymail@gmail.com>
2024-08-11 06:57:58 -0600

    underflow of a stack var is not caught

--- broken.c
+++ broken.c
@@ -6,9 +6,9 @@
 // Exploring memory leaks, overflows, and underflows.
 
 
-
 int broken(){
 	char *dyn;
+	char local[5];
 
 	// overwrite buffer a scosche
 	// dyn = malloc(5);
@@ -27,13 +27,30 @@ int broken(){
 	// Improvements in the compiler since Linux Application Development
 	// was published in 2004? Book says it compiles and runs.
 
-	// walk past the beginning of a malloced buffer
+	// walk past the beginning of a malloced buffer (underflow)
 
-	*(dyn - 1) = '\0';
+	// *(dyn - 1) = '\0';
 
 	// Result
-
 	// Seg fault
+
+	// overflow local var (stack variable)
+
+	// strcpy(local, "12345");
+
+	// Result
+	// 	broken.c: In function ‘broken’:
+	// broken.c:40:9: warning: ‘__builtin_memcpy’ writing 6 bytes into a region of size 5 overflows the destination [-Wstringop-overflow=]
+	//    40 |         strcpy(local, "12345");
+	//       |         ^~~~~~~~~~~~~~~~~~~~~~
+	// broken.c:12:14: note: destination object ‘local’ of size 5
+	//    12 |         char local[5];
+	//       |              ^~~~~
+
+	// underflow of stack var is not caught
+	strcpy(local, "1234");
+	local[-1] = '\0';
+	printf("%s\n", local);
 
 	return 0;
 }
