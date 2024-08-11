1f4277742d2fc1ef5ad39f4d6da02e9649f8aab3
SK <transiencymail@gmail.com>
2024-08-11 06:51:22 -0600

    Linux catches underflows in a malloc'ed buffer

--- broken.c
+++ broken.c
@@ -5,25 +5,42 @@
 // Broken.c
 // Exploring memory leaks, overflows, and underflows.
 
-// broken.c: In function ‘broken’:
-// broken.c:14:9: warning: ‘__builtin_memcpy’ writing 6 bytes into a region of size 5 overflows the destination [-Wstringop-overflow=]
-//    14 |         strcpy(dyn, "12345");
-//       |         ^~~~~~~~~~~~~~~~~~~~
-// broken.c:13:15: note: destination object of size 5 allocated by ‘malloc’
-//    13 |         dyn = malloc(5);
-//       |               ^~~~~~~~~
 
-// Improvements in the compiler since Linux Application Development
-// was published in 2004?
 
 int broken(){
 	char *dyn;
 
 	// overwrite buffer a scosche
-	dyn = malloc(5);
-	strcpy(dyn, "12345");
+	// dyn = malloc(5);
+	// strcpy(dyn, "12345");
+
+	// Result
+
+	// broken.c: In function ‘broken’:
+	// broken.c:14:9: warning: ‘__builtin_memcpy’ writing 6 bytes into a region of size 5 overflows the destination [-Wstringop-overflow=]
+	//    14 |         strcpy(dyn, "12345");
+	//       |         ^~~~~~~~~~~~~~~~~~~~
+	// broken.c:13:15: note: destination object of size 5 allocated by ‘malloc’
+	//    13 |         dyn = malloc(5);
+	//       |               ^~~~~~~~~
+
+	// Improvements in the compiler since Linux Application Development
+	// was published in 2004? Book says it compiles and runs.
+
+	// walk past the beginning of a malloced buffer
+
+	*(dyn - 1) = '\0';
+
+	// Result
+
+	// Seg fault
+
+	return 0;
 }
+
+
 
 int main(){
 	return broken();
 }
+
